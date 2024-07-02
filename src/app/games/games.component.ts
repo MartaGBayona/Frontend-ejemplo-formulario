0183bcbaf5cea6429e364b075a37d0389cca1187
Marta Gimeno Bayona <marta.g.bayona@outlook.es>
2024-07-02 14:55:27 +0200

    feat: creacion de una iteracion utilizando la sintaxis @for

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -4,9 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-games',
   standalone: true,
   imports: [],
-  templateUrl: './games.component.html',
-  styleUrl: './games.component.css'
+  template: `
+  <ul>
+    @for (game of games; track game.id){ 
+
+    }
+</ul>
+  `,
+  styles: `
+  `
 })
 export class GamesComponent {
-
+games = [
+  {
+    id: 1,
+    name: 'Uncharted'
+  },
+  {
+    id: 2,
+    name: 'Horizon'
+  },
+  {
+    id: 3,
+    name: 'Diablo'
+  },
+]
 }
